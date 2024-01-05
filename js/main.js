844c530f92d4e440a16e3d9181a4a0d93a95ee8b
Islomjon0221 <islomjon20110221@gmail.com>
2024-01-05 20:53:32 +0500

    Uyga vazifa chala qilindi

---
+++ js/main.js
@@ -0,0 +1,111 @@
+const todoHeader = document.getElementById('todo-header');
+const input = document.querySelector('#input');
+const button = document.querySelector('#button');
+const error = document.querySelector('#error');
+const todoList = document.querySelector('#todo-list');
+
+function validate(form, error) {
+    if(!form.value) {
+        error.style.display = 'block'
+        return false;
+    } else {
+        error.style.display = 'none'
+    }
+
+    if (!form.value.trim()) {
+        error.style.display = 'block'
+        error.innerHTML = "Matn probellardan iborat bo'lmasligi shart"
+        return false;
+    } else {
+        error.style.display = 'none'
+        error.innerHTMl = "Matn kiritilishi shart"
+    }
+
+    if(form.value.length <= 4) {
+        error.style.display = 'block'
+        error.innerHTML = "Belgilar soni kamida 5 ta bo'lishi kerak"
+        return false;
+    } else {
+        error.style.display = 'none'
+        error.innerHTMl = "Matn kiritilishi shart"
+    }
+
+    return true;
+}
+
+button && button.addEventListener('click', function() {
+    if(validate(input, error)) {
+        const todo = {
+            id: Date.now(),
+            name: input.value,
+            status: "active"
+        };
+
+        let data = [];
+        if(localStorage.getItem('todos')) {
+            data = JSON.parse(localStorage.getItem('todos'));
+        }
+
+        data.push(todo);
+        localStorage.setItem('todos', JSON.stringify(data));
+        let todoItem = createTodo(todo);
+        todoList.innerHTML += todoItem;
+        input.value = '';
+    } else {
+        console.log('Validatsiya o`tmadi')
+    }
+});
+
+input && input.addEventListener('keyup', function(event) {
+    if(event.keyCode == 13) {
+        if(validate(input, error)) {
+            const todo = {
+                id: Date.now(),
+                name: input.value,
+                status: "active"
+            };
+    
+            let data = [];
+            if(localStorage.getItem('todos')) {
+                data = JSON.parse(localStorage.getItem('todos'));
+            }
+    
+            data.push(todo);
+            localStorage.setItem('todos', JSON.stringify(data));
+            let todoItem = createTodo(todo);
+            todoList.innerHTML += todoItem;
+            input.value = '';
+        } else {
+            console.log('Validatsiya o`tmadi')
+        }
+    }
+});
+
+function getDataFromLStorage() {
+    let data = [];
+    if(localStorage.getItem('todos')) {
+        data = JSON.parse(localStorage.getItem('todos'))
+    }
+
+    return data;
+}
+
+function createTodo(todo) {
+    let status = false;
+    if(!todo.status == 'active') {
+        status = true;
+    } 
+    return `
+    <li data-item = "todo_${todo.id}">
+        <div class="check-name">
+            <input type="checkbox" name="" id="" ${status ? 'checked' : ''}>
+            <p>${todo.name}</p>
+        </div>
+
+        <div class="actions">
+            <i class="fa-regular fa-pen-to-square"></i>
+            <i id="${todo.id}" class="fa-regular fa-trash-can"></i>
+        </div>
+    </li>
+`;
+}
